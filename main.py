6157e5160a5c87f51cfd015c6141f9378bfd06c5
Varun Jindal <vjindz@gmail.com>
2017-06-16 10:58:17 -0400

    started creating sprite group for enemies

--- main.py
+++ main.py
@@ -23,8 +23,27 @@ class ShooterMain:
 		
 	def loadSprites(self):
 		self.soldier = Soldier(100)
-		self.enemy = Enemy(10, [self.width, self.height], 10)
+		self.enemy = pygame.sprite.Group()
+		#self.enemy = Enemy(10, [self.width, self.height], 10)
 		self.bullet = pygame.sprite.Group()
+		
+	def findVector(self, firstPos, secondPos):
+		ydiff = (firstPos[1] - secondPos[1])
+		xdiff = (firstPos[0] - secondPos[0])
+		if(ydiff == 0):
+			ydiff = .01
+		if(xdiff == 0):
+			xdiff = .01
+		angle = math.atan(xdiff/ydiff)
+		if(xdiff>0 and ydiff>0 or xdiff<0 and ydiff>0):
+			angle += math.pi
+		xDir = xdiff/abs(xdiff) * .5
+		yDir = ydiff/abs(ydiff) * .5
+		if(ydiff<2 and ydiff>-2):
+			yDir = 0
+		if(xdiff<2 and xdiff>-2):
+			xDir = 0
+		return [xDir,yDir,angle]
 		
 	def mainLoop(self):
 		self.loadSprites()
@@ -42,12 +61,15 @@ class ShooterMain:
 				if event.type == pygame.KEYDOWN:
 					if event.key == K_SPACE:
 						self.bullet.add(Bullet(self.soldier.angle, self.soldier.rect))
+						self.enemy.add(Enemy(10, [self.width, self.height], 10))
 				if event.type == pygame.QUIT:
 					sys.exit()
 			self.screen.fill((100,180,180))
 			self.screen.blit(self.soldier.image, self.soldier.rect)
-			self.enemy.updatePos(self.soldier.findVector(self.enemy.enemyPos))
-			self.screen.blit(self.enemy.image, self.enemy.rect)
+			for x in self.enemy.sprites():
+				x.update(self.findVector(self.soldier.pos, x.enemyPos))
+			self.enemy.draw(self.screen)
+			#self.screen.blit(self.enemy.image, self.enemy.rect)
 			#if(self.bullet.has()):
 			self.bullet.update()
 			self.bullet.draw(self.screen)
@@ -87,25 +109,6 @@ class Soldier(pygame.sprite.Sprite):
 		
 	def calcPointList(self, x, y):
 		self.points = [[x, y+10],[x-5,y-5],[x+5,y-5]] 
-		
-	def findVector(self, pos):
-		ydiff = (self.pos[1] - pos[1])
-		xdiff = (self.pos[0] - pos[0])
-		if(ydiff == 0):
-			ydiff = .01
-		if(xdiff == 0):
-			xdiff = .01
-		angle = math.atan(xdiff/ydiff)
-		if(xdiff>0 and ydiff>0 or xdiff<0 and ydiff>0):
-			angle += math.pi
-		xDir = xdiff/abs(xdiff) * .5
-		yDir = ydiff/abs(ydiff) * .5
-		if(ydiff<2 and ydiff>-2):
-			yDir = 0
-		if(xdiff<2 and xdiff>-2):
-			xDir = 0
-		return [xDir,yDir,angle]
-		#FINISH
 	
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self, health, screenDim, angle):
@@ -119,7 +122,7 @@ class Enemy(pygame.sprite.Sprite):
 		self.enemyPos = [random.choice(xrange), random.choice(yrange)] 
 		self.rect = self.enemyPos
 	
-	def updatePos(self, vector):
+	def update(self, vector):
 		self.enemyPos = [self.enemyPos[0] + vector[0] * 1.3, self.enemyPos[1] + vector[1] * 1.3]
 		self.angle = vector[2]*180/math.pi
 		self.image = pygame.transform.rotozoom(enemyMasterImg, self.angle + 45, 1)
@@ -133,8 +136,8 @@ class Bullet(pygame.sprite.Sprite):
 		self.rect = [pos[0] - math.cos(self.angle*math.pi/180)*30 + 12, pos[1] + math.sin(self.angle*math.pi/180)*25 + 10] # implemented circle formula
 		
 	def update(self):
-		self.x = math.cos((self.angle+6)*math.pi/180 + math.pi) * 2.5
-		self.y = math.sin((self.angle+6)*math.pi/180 + math.pi) * 2.5
+		self.x = math.cos((self.angle)*math.pi/180 + math.pi) * 2.5
+		self.y = math.sin((self.angle)*math.pi/180 + math.pi) * 2.5
 		self.image = pygame.transform.rotozoom(bulletMasterImg, self.angle + 180, 1)
 		self.rect = [self.rect[0] + self.x, self.rect[1] - self.y]
 		if (self.rect[0] > 640 or self.rect[0] < 0 or self.rect[1] > 480 or self.rect[1] < 0):
